ad1a4063b97c990c87a44e4426b152a3239bec69
Tomosige <tomoterranite@gmail.com>
2019-08-22 01:16:06 +0900

    Tapiocraftセットアップ完了

---
+++ tapiocraft/src/main/java/tapiocraft/tom/jp/Tapiocraft.java
@@ -0,0 +1,12 @@
+package tapiocraft.tom.jp;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = Tapiocraft.MOD_ID,name = Tapiocraft.MOD_NAME, version = Tapiocraft.MOD_VERSION,dependencies = Tapiocraft.MOD_DEPENDENCIES,acceptedMinecraftVersions = Tapiocraft.MOD_ACCEPTED_MC_VERSIONS)
+public class Tapiocraft {
+    public static final String MOD_ID = "tapiocraft";
+    public static final String MOD_NAME = "Tapiocraft";
+    public static final String MOD_VERSION = "0.0.1";
+    public static final String MOD_DEPENDENCIES = "required-after:forge@[1.12.2-14.23.5.2838,]";
+    public static final String MOD_ACCEPTED_MC_VERSIONS = "[1.12.2]";
+}
